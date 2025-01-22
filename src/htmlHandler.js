b8d32b9d1eb4eb0bc4ff41bc3791f90790e28af7
ZakhaevK <zkupke@gmail.com>
2025-01-22 19:41:52 +1100

    add search button event to call getGif()

---
+++ src/htmlHandler.js
@@ -0,0 +1,15 @@
+import {getGif} from "./apiHandler.js";
+export {initialiseApp};
+
+const searchBar = document.getElementById("search-addr");
+const searchButt = document.getElementById("search-butt");
+const weatherGif = document.getElementById("weather-gif");
+
+
+async function initialiseApp() {
+  searchButt.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const gif = await getGif("placeholder");
+    weatherGif.src = gif;
+  })
+}
